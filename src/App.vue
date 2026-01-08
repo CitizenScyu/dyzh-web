255f2aa27bd5441b51f0c3b5fe85afa187c211a9
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2026-01-08 08:24:48 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    const script = document.createElement('script')
-    script.defer = true
-    script.src = 'https://umami.cloud.us.kg/script.js'
-    script.setAttribute('data-website-id', 'dd9c6c3e-4ae7-4c7d-9684-6021e9325880')
-    document.head.appendChild(script)
-  }
-}
-</script>
